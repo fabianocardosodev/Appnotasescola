a8e92ddccd56f5d751c5ba20fe16595a44e5e114
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-11-09 07:31:50 -0200

    atualização base.html

--- appnotasescola/appnotasescola/settings.py
+++ appnotasescola/appnotasescola/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = ''
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/Appnotasescola/appnotasescola/appnotasescola/static'),
+    os.path.join(BASE_DIR, '/Appnotasescola/appnotasescola/appnotasescola/static/'),
 )
