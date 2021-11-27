bb687635bfe74c67ed10941005d99b8b9800db4f
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-11-27 00:07:24 -0200

    alteração home

--- appnotasescola/appnotasescola/urls.py
+++ appnotasescola/appnotasescola/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 
 
@@ -22,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('principal.urls', namespace="principal")),
     path('accounts/', include("allauth.urls")),
-    
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     
     
 ]
